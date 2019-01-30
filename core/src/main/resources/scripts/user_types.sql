5fff62bcc58e6281f44dd4b4381766f69081c7b1
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-30 15:21:18 +0100

    other schema changes.

--- core/src/main/resources/scripts/user_types.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_type(user_type) VALUES ('MEMBER');
-INSERT INTO user_type(user_type) VALUES ('MANAGER');
-INSERT INTO user_type(user_type) VALUES ('TEAM_LEADER');
-INSERT INTO user_type(user_type) VALUES ('ADMIN');
-INSERT INTO user_type(user_type) VALUES ('SUPERADMIN');
