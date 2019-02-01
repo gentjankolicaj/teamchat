262fda19f255813399a89c35fc253d6a3d46f81e
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-01 11:42:52 +0100

    some script changes.

--- src/main/resources/scripts/groups.sql
+++ src/main/resources/scripts/groups.sql
@@ -5,20 +5,3 @@
 INSERT INTO `group`(name) VALUES ('ORGANIZATION');
 INSERT INTO `group`(name) VALUES ('DEPARTMENT');
 INSERT INTO `group`(name) VALUES ('TEAM');
-
-
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (1,1);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (1,2);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (1,3);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (1,4);
-
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (2,1);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (2,2);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (2,3);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (2,4);
-
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (3,1);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (3,2);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (3,3);
-INSERT INTO group_privilege (group_id,privilege_id) VALUES (3,4);
-
