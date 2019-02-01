c262a96a8b32a364b2e4a9f834d1ccd5a0157e0b
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-01 16:15:06 +0100

    exception class renaming.

--- src/main/java/teamchat/exception/GroupException.java
+++ src/main/java/teamchat/exception/GroupException.java
@@ -4,29 +4,29 @@ package teamchat.exception;
  * @author gentjan kolicaj
  *
  */
-public class GroupExcetion extends RuntimeException {
+public class GroupException extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -85706797740761605L;
 
-	public GroupExcetion() {
+	public GroupException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public GroupExcetion(String message, Throwable cause) {
+	public GroupException(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public GroupExcetion(String message) {
+	public GroupException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public GroupExcetion(Throwable cause) {
+	public GroupException(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
