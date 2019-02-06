69a2f697252a697c29c8c6caf6203001040fb4ab
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-06 17:37:15 +0100

    impl swagger into project.

--- src/main/java/teamchat/web/config/WebMvcConfig.java
+++ src/main/java/teamchat/web/config/WebMvcConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer{
 	
 	
 	@Bean
@@ -20,5 +27,14 @@ public class WebMvcConfig {
 		return viewResolver;
 	}
 
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 	
 }
