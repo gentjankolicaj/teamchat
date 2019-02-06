f599cd678a5e4b627a074d89fbd91b557f37cea5
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-06 17:21:47 +0100

    new exception class.

---
+++ src/main/java/teamchat/exception/request/ParamException.java
@@ -0,0 +1,34 @@
+package teamchat.exception.request;
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
+public class ParamException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1012436198875445916L;
+
+	public ParamException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParamException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParamException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParamException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
