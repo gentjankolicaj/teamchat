33b87804c7cb8d533f78415d02f3dfac7a246064
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-13 15:38:27 +0100

    new view resources,and controller.

---
+++ src/main/java/teamchat/web/controllers/ControlPanelController.java
@@ -0,0 +1,42 @@
+package teamchat.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import teamchat.data.domain.User;
+
+/**
+ * 
+ * @author gentjan kolicaj
+ *
+ */
+@Controller
+@RequestMapping(ControlPanelController.URI)
+public class ControlPanelController {
+
+	protected final static String URI="/cpanel";
+
+	public ControlPanelController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@RequestMapping(path= {"","/","/**"})
+	public String showControlPanel(Model model) {
+		
+		User user=new User();
+		
+		model.addAttribute("user",user);
+		
+		return "control-panel";
+	}
+
+	
+	@RequestMapping(path= {"/out"})
+	public String logoutFromControlPanel() {
+		System.out.println("Redirecting at signin");
+		return "redirect:/signin";
+	}
+}
