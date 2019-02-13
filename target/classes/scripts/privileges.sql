f83fc346b8d17d32da6e69e8f7be2c4e554f0c0f
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-13 18:08:56 +0100

    mvn clean.

--- target/classes/scripts/privileges.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO privilege(identifier,description) VALUES ('CREATE','Create privilege for any action');
-INSERT INTO privilege(identifier,description) VALUES ('READ','Create privilege for any action');
-INSERT INTO privilege(identifier,description) VALUES ('UPDATE','Create privilege for any action');
-INSERT INTO privilege(identifier,description) VALUES ('DELETE','Create privilege for any action');
